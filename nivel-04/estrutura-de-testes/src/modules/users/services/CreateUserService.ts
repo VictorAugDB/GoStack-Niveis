3ae8926e04cb8f01e6bd655766bd0211ae175fd5
Victor Augusto <vdbernardes@gmail.com>
2020-12-17 20:33:30 -0300

    Liskov, Dependency Inversion, Reescrevendo repositórios, Refatorando módulo de usuários.

--- nivel-04/estrutura-de-testes/src/modules/users/services/CreateUserService.ts
+++ nivel-04/estrutura-de-testes/src/modules/users/services/CreateUserService.ts
@@ -1,23 +1,21 @@
-import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
 import AppError from '@shared/errors/appError';
+import IUsersRepository from '../repositories/IUsersRepository';
 
-interface Request {
+interface IRequest {
   name: string;
   email: string;
   password: string;
 }
 
 class CreateUserService {
-  async execute({ name, email, password }: Request): Promise<User> {
-    const usersRepository = getRepository(User);
+  constructor(private usersRepository: IUsersRepository) {}
 
-    const checkUserExists = await usersRepository.findOne({
-      where: { email },
-    });
+  async execute({ name, email, password }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(email);
 
     if (checkUserExists) {
       throw new AppError('Email adress already used');
@@ -25,13 +23,13 @@ class CreateUserService {
 
     const hashedPassword = await hash(password, 8);
 
-    const user = usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
     });
 
-    await usersRepository.save(user);
+    await this.usersRepository.save(user);
 
     return user;
   }
