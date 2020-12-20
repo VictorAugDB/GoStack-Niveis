15e8d4836ff9c05dd8ad6214677a98d7ee519d16
Victor Augusto <vdbernardes@gmail.com>
2020-12-20 15:46:08 -0300

    testando criação de usuário, inicio criando testes de autenticação.

---
+++ nivel-04/estrutura-de-testes/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -0,0 +1,25 @@
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+import AuthenticateUserService from './AuthenticateUserService';
+
+describe('AuthenticateUser', () => {
+  it('should be able to authenticate', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const authenticateUser = new AuthenticateUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const response = await authenticateUser.execute({
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    expect(response).toHaveProperty('token');
+  });
+});
